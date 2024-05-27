7d69766fe9b1d046f52f3540a02620f5f8dda893
Pugemon <islavaivanov76@gmail.com>
2024-05-27 19:05:09 +0300

    Refactor code

--- source/main.c
+++ source/main.c
@@ -4,58 +4,62 @@
 #include <string.h>
 #include <stdio.h>
 
-bool UtilsIsN3Ds(void) {
-  bool isNew3DS = false;
-  return R_SUCCEEDED(APT_CheckNew3DS(&isNew3DS)) && isNew3DS;
+
+// [[nodiscard]] attribute indicates that the return value of the function should not be ignored.
+[[nodiscard]] bool UtilsIsN3Ds(void) {
+  bool is_new_3ds = false;
+  return R_SUCCEEDED(APT_CheckNew3DS(&is_new_3ds)) && is_new_3ds;
 }
 
-char *SystemGetScreenType(void) {
-  static char upperScreen[20];
-  static char lowerScreen[20];
-
-  static char screenType[32];
+char* SystemGetScreenType(void) {
+  static char upper_screen[20];
+  static char lower_screen[20];
+  static char screen_type[32];
 
   if (UtilsIsN3Ds()) {
     u8 screens = 0;
 
     if (R_SUCCEEDED(gspLcdInit())) {
-      if (R_SUCCEEDED(GSPLCD_GetVendors(&screens)))
-        gspLcdExit();
+      if (R_SUCCEEDED(GSPLCD_GetVendors(&screens))) gspLcdExit();
     }
 
     switch ((screens >> 4) & 0xF) {
-      case 0x01: // 0x01 = JDI => IPS
-        sprintf(upperScreen, "Upper: IPS");
+      case 0x01:  // 0x01 = JDI => IPS
+        sprintf(upper_screen, "Upper: IPS");
         break;
-      case 0x0C: // 0x0C = SHARP => TN
-        sprintf(upperScreen, "Upper: TN");
+      case 0x0C:  // 0x0C = SHARP => TN
+        sprintf(upper_screen, "Upper: TN");
         break;
-      default:sprintf(upperScreen, "Upper: Unknown");
+      default:
+        sprintf(upper_screen, "Upper: Unknown");
         break;
     }
+
     switch (screens & 0xF) {
-      case 0x01: // 0x01 = JDI => IPS
-        sprintf(lowerScreen, " | Lower: IPS");
+      case 0x01:  // 0x01 = JDI => IPS
+        sprintf(lower_screen, " | Lower: IPS");
         break;
-      case 0x0C: // 0x0C = SHARP => TN
-        sprintf(lowerScreen, " | Lower: TN");
+      case 0x0C:  // 0x0C = SHARP => TN
+        sprintf(lower_screen, " | Lower: TN");
         break;
-      default:sprintf(lowerScreen, " | Lower: Unknown");
+      default:
+        sprintf(lower_screen, " | Lower: Unknown");
         break;
     }
 
-    strcpy(screenType, upperScreen);
-    strcat(screenType, lowerScreen);
-  } else
-    sprintf(screenType, "Upper: TN | Lower: TN");
+    strcpy(screen_type, upper_screen);
+    strcat(screen_type, lower_screen);
+  } else {
+    sprintf(screen_type, "Upper: TN | Lower: TN");
+  }
 
-  return screenType;
+  return screen_type;
 }
 
-u8 batteryPercent = 0;
-u8 batteryStatus = 0;
-bool isConnected = false;
-bool isBottomScreen = false;
+u8 battery_percent = 0;
+u8 battery_status = 0;
+bool is_connected = false;
+bool is_bottom_screen = false;
 
 void PrintStaticInfo(void) {
   printf("\x1b[1;0H\x1b[32mGreenCitrus\x1b[0m \nTool for de-yellow'ing TN screens");
@@ -65,100 +69,104 @@ void PrintStaticInfo(void) {
   printf("\x1b[10;0H\x1b[32m*\x1b[0m Selected screen:");
   printf("\x1b[11;0HPress \x1b[32mUP\x1b[0m or \x1b[32mDOWN\x1b[0m to select screen");
   printf("\x1b[30;0HPress\x1b[31;1m START\x1b[0m to exit");
-
 }
 
 void PrintDynamicInfo(void) {
-  if (R_SUCCEEDED(MCUHWC_GetBatteryLevel(&batteryPercent)))
-    printf("\x1b[6;22H\x1b[34;1m%3d%%\x1b[0m ", batteryPercent);
+  if (R_SUCCEEDED(MCUHWC_GetBatteryLevel(&battery_percent)))
+    printf("\x1b[6;22H\x1b[34;1m%3d%%\x1b[0m ", battery_percent);
   else
     printf("\x1b[34;1mN/A\x1b[0m ");
 
-  if (R_SUCCEEDED(PTMU_GetBatteryChargeState(&batteryStatus)))
-    printf("(\x1b[34;1m%s\x1b[0m)     \n", batteryStatus ? "charging" : "not charging");
+  if (R_SUCCEEDED(PTMU_GetBatteryChargeState(&battery_status)))
+    printf("(\x1b[34;1m%s\x1b[0m)     \n", battery_status ? "charging" : "not charging");
   else
     printf("(\x1b[34;1mN/A\x1b[0m)\n");
 
   printf("\x1b[8;18H");
-  if (R_SUCCEEDED(PTMU_GetAdapterState(&isConnected)))
-    printf("\x1b[34;1m%s\x1b[0m\n", isConnected ? "connected   " : "disconnected");
+  if (R_SUCCEEDED(PTMU_GetAdapterState(&is_connected)))
+    printf("\x1b[34;1m%s\x1b[0m\n", is_connected ? "connected   " : "disconnected");
   else
     printf("\x1b[34;1mN/A\x1b[0m\n");
 
-  printf("\x1b[10;19H \x1b[32m>\x1b[0m%s", isBottomScreen ? "bottom" : "top");
+  printf("\x1b[10;19H \x1b[32m>\x1b[0m%s", is_bottom_screen ? "bottom" : "top");
 }
 
-void DrawFrame(C3D_RenderTarget *screen, u32 color) {
+void DrawFrame(C3D_RenderTarget* screen, u32 color) {
   C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
   C2D_TargetClear(screen, color);
   C2D_SceneBegin(screen);
   C3D_FrameEnd(0);
 }
 
-int main() {
+void InitServices(void) {
+  mcuHwcInit();  // Battery info
+  ptmuInit();    // Charger info
+  gspLcdInit();  // Screen brightness
+}
 
-  // Init libs
+void TerminateServices(void) {
+  ptmuExit();    // Battery info
+  mcuHwcExit();  // Charger info
+  gspLcdExit();  // Screen brightness
+}
+
+int main(void) {
+  // Initialize libraries.
   gfxInitDefault();
   C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
   C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
   C2D_Prepare();
-  PrintConsole consoleTop, consoleBottom;
-  consoleInit(GFX_TOP, &consoleTop);
-  consoleInit(GFX_BOTTOM, &consoleBottom);
-  mcuHwcInit(); // Battery info
-  ptmuInit(); // Charger info
-  gspLcdInit(); // Screen brightness
+  PrintConsole console_top, console_bottom;
+  consoleInit(GFX_TOP, &console_top);
+  consoleInit(GFX_BOTTOM, &console_bottom);
+  InitServices();
   GSPLCD_SetBrightness(GSPLCD_SCREEN_BOTH, 5);
 
+  // Create screens.
+  C3D_RenderTarget* top = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
+  C3D_RenderTarget* bot = C2D_CreateScreenTarget(GFX_BOTTOM, GFX_LEFT);
+  C3D_RenderTarget* screen = top;
 
-  // Create screens
-  C3D_RenderTarget *top = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
-  C3D_RenderTarget *bot = C2D_CreateScreenTarget(GFX_BOTTOM, GFX_LEFT);
-  C3D_RenderTarget *screen = top;
+  // Create colors.
+  u32 clr_white = C2D_Color32(0xFF, 0xFF, 0xFF, 0xFF);
+  u32 clr_black = C2D_Color32(0x00, 0x00, 0x00, 0x00);
 
-  // Create colors
-  u32 clrWhite = C2D_Color32(0xFF, 0xFF, 0xFF, 0xFF);
-  u32 clrBlack = C2D_Color32(0x00, 0x00, 0x00, 0x00);
-
-  DrawFrame(screen, clrWhite);
-  consoleSelect(&consoleBottom);
+  DrawFrame(screen, clr_white);
+  consoleSelect(&console_bottom);
   PrintStaticInfo();
 
-  // Main loop
+  // Main loop.
   while (aptMainLoop()) {
     hidScanInput();
 
-    // Respond to user input
-    u32 kDown = hidKeysDown();
-    if (kDown & KEY_START)
-      break; // break in order to return to hbmenu
+    // Respond to user input.
+    u32 k_down = hidKeysDown();
+    if (k_down & KEY_START) break;  // Break to return to hbmenu.
 
     PrintDynamicInfo();
 
-    if (kDown & KEY_DDOWN) {
-      DrawFrame(screen, clrBlack);
+    if (k_down & KEY_DDOWN) {
+      DrawFrame(screen, clr_black);
       screen = bot;
-      isBottomScreen = true;
-      consoleSelect(&consoleTop);
+      is_bottom_screen = true;
+      consoleSelect(&console_top);
       PrintStaticInfo();
-      DrawFrame(screen, clrWhite);
+      DrawFrame(screen, clr_white);
     }
-    if (kDown & KEY_DUP) {
-      DrawFrame(screen, clrBlack);
+    if (k_down & KEY_DUP) {
+      DrawFrame(screen, clr_black);
       screen = top;
-      isBottomScreen = false;
-      consoleSelect(&consoleBottom);
+      is_bottom_screen = false;
+      consoleSelect(&console_bottom);
       PrintStaticInfo();
-      DrawFrame(screen, clrWhite);
+      DrawFrame(screen, clr_white);
     }
   }
 
-  // Deinit libs
+  // Deinitialize libraries.
   C2D_Fini();
   C3D_Fini();
-  ptmuExit();
-  mcuHwcExit();
-  gspLcdExit();
+  TerminateServices();
   gfxExit();
   return 0;
 }
